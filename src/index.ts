753c61828a586e29bbcf0403d8960664e5117428
Nao <dragonarcher2005@gmail.com>
2023-11-29 03:03:10 -0300

    Vercel cors

--- src/index.ts
+++ src/index.ts
@@ -21,5 +21,16 @@ app.listen(PORT, () =>{
     console.log(`server running on port ${PORT}`);
     
 })
-
+app.use('/api/diaries', (_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Puedes restringir esto a tu dominio en producción
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    
+    // Preflight request, responde con 200 OK
+    if (_req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+});
 app.use('/api/diaries', diaryRouter)
