cc67b82d54ca8e8443abf1176072e7f96fc48ce0
NaoRYK <dragonarcher2005@gmail.com>
2023-11-29 17:31:33 -0300

    Adding cors build

--- build/index.js
+++ build/index.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const diaries_1 = __importDefault(require("./routes/diaries"));
 const app = (0, express_1.default)();
+const cors = require("cors");
+app.use(cors()); // Use this after the variable declaration
 app.use(express_1.default.json());
 const PORT = 3000;
 app.get("/ping", (_req, res) => {
@@ -14,5 +16,17 @@ app.get("/ping", (_req, res) => {
 });
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
+});
+app.use('/api/diaries', (_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Puedes restringir esto a tu dominio en producción
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // Preflight request, responde con 200 OK
+    if (_req.method === 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 });
 app.use('/api/diaries', diaries_1.default);
