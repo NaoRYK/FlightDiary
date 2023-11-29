59d50c9473f3fe1b42d48539135f14c81de04104
Nao <dragonarcher2005@gmail.com>
2023-11-29 02:50:50 -0300

    try

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,11 @@ import diaryRouter from './routes/diaries'
 const app = express()
 const cors=require("cors");
 
-
-app.use(cors()) // Use this after the variable declaration
+const corsOptions = {
+    origin: "http://127.0.0.1:5500",
+  };
+  
+app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(express.json())
 
 const PORT = 3000
